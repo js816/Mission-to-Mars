567f6ea7f101184f6a48fc4a10646de2e2198843
unknown <82730954+js816@users.noreply.github.com>
2021-07-03 14:51:35 -0500

    Adding updated index.html file for deliverable 3

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import scraping
 # Setting up Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up a mongo connection
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Adding the index route
